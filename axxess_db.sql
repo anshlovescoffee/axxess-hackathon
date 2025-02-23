1ae3d63709b35e1301713ffe2535dfd4c81e3e3b
Marc Manoj <sirmarc1598@gmail.com>
2025-02-22 20:42:08 -0600

    waaaaaa waaaaaaa fml  Please enter the commit message for your changes. Lines starting

--- axxess_db.sql
+++ axxess_db.sql
@@ -6,7 +6,8 @@ CREATE TABLE Patients (
     SSN INT,
     Address VARCHAR(50),
     Zipcode INT,
-    State_Abv CHAR(2)
+    State_Abv CHAR(2),
+    phone_num VARCHAR(10)
 );
 CREATE TABLE Patient_measurements (
 	PID INT PRIMARY KEY,
@@ -27,7 +28,6 @@ CREATE TABLE Visitation_log (
     Heart_Rate INT,
     bp_systolic INT,
     bp_diastolic INT,
-    measurement_time TIMESTAMP,
     CONSTRAINT systolic_check CHECK (bp_systolic BETWEEN 60 AND 250),
     CONSTRAINT diastolic_check CHECK (bp_diastolic BETWEEN 40 AND 180),
     FOREIGN KEY (PID) REFERENCES Patients(PID)
@@ -50,14 +50,3 @@ CREATE TABLE Patient_Meds (
     FOREIGN KEY (Med_Name) REFERENCES Med(Med_Name)
 );
 
-
-INSERT INTO Patients (PID, P_Name, SSN, Address, Zipcode, State_Abv) 
-VALUES ( 1, "Jude Joubert", 435762835, "865 Greenside Dr.", 75080, 'TX');
-INSERT INTO Patient_measurements (PID, Age, Sex, Weight, Height) 
-VALUES (1, 70, 1, 180, 68);
-INSERT INTO Visitation_log (PID, N_Name, P_Name, Date_of_visitation, Notes, Respiratory_Rate, Heart_Rate, bp_systolic, bp_diastolic, measurement_time) 
-VALUES (1, 'Nurse A', 'John Doe', '2023-10-01', 'Routine checkup', 16, 75, 120, 80, NOW());
-INSERT INTO Med (Med_Name, Total_Amount, Unit) 
-VALUES ('Morphine', 20.00, 'mg');
-INSERT INTO Patient_Meds (PID, Med_Name, Dosage, Unit, Remaining, Exp_Date) 
-VALUES (1, 'Morphine', 5.00, 'mg', 10, '2028-10-01');
